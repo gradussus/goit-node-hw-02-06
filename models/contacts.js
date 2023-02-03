111cd3c23065bd73f559e728616fa0bf273c7262
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2023-02-03 13:46:26 +0200

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,8 +1,17 @@
-// const fs = require('fs/promises')
+const fs = require('fs').promises
+const path = require('path')
+const contactsPath = path.resolve('./models.contacts.json')
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath)
+  return JSON.parse(contacts)
+}
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  contacts = await listContacts()
+  const [desiredContacts] = c => c.id === contactId
+  return desiredContacts
+}
 
 const removeContact = async (contactId) => {}
 
