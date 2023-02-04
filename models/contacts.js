2346ed6a9ef1bc954f509e654f1820f6484cb42b
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2023-02-04 11:05:26 +0200

    1

--- models/contacts.js
+++ models/contacts.js
@@ -2,7 +2,7 @@ const fs = require("fs").promises;
 const path = require("path");
 const { v4 } = require("uuid");
 
-const contactsPath = path.resolve("./models.contacts.json");
+const contactsPath = path.resolve("./models/contacts.json");
 
 const listContacts = async () => {
   try {
@@ -38,32 +38,31 @@ const removeContact = async (contactId) => {
 
 const addContact = async ({ name, email, phone }) => {
   if (!name || !email || !phone) {
-    return false  
+    return false;
   }
   try {
     const contacts = await listContacts();
-    const newContact = { name, email, phone, id: v4() }
-    contacts.push(newContact)
-    await fs.writeFile( contactsPath, JSON.stringify(contacts))
-    return newContact 
-
+    const newContact = { name, email, phone, id: v4() };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return newContact;
   } catch (err) {
     console.error(err);
   }
 };
 
-const updateContact = async (contactId, {name, email, phone}) => {
+const updateContact = async (contactId, { name, email, phone }) => {
   try {
     const contacts = await listContacts();
-    contacts.forEach(async cont => {
+    contacts.forEach(async (cont) => {
       if (cont.id === contactId) {
-        cont.name = name
-        cont.email = email
-        cont.phone = phone
-        await fs.writeFile(contactsPath, JSON.stringify(contacts))
-        return cont
+        cont.name = name;
+        cont.email = email;
+        cont.phone = phone;
+        await fs.writeFile(contactsPath, JSON.stringify(contacts));
+        return cont;
       }
-      return false
+      return false;
     });
   } catch (err) {
     console.error(err);
