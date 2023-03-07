02d2ead2274c227f622cdd1208ff8a2ed1f1e801
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2023-03-07 14:57:40 +0200

    №1

--- schemas/userModel.js
+++ schemas/userModel.js
@@ -25,6 +25,14 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
   },
   { versionKey: false }
 );
